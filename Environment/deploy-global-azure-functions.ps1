8d83e36b515c1add5c95a0348102e8f1be058618
Daniel Bass <danbass8@googlemail.com>
2020-08-07 23:59:31 +0100

    added csharp to deployment script

--- Environment/deploy-global-azure-functions.ps1
+++ Environment/deploy-global-azure-functions.ps1
@@ -5,5 +5,5 @@ param(
 $Regions = "eastasia","southeastasia","centralus","eastus", "westus", "northcentralus", "southcentralus",  "northeurope","westeurope", "japaneast", "brazilsouth", "australiasoutheast", "canadacentral", "uksouth", "southafricanorth"
 
 $Regions | ForEach-Object -Parallel {
-    func azure functionapp publish "$($using:Name)$($_)"
+    func azure functionapp publish "$($using:Name)$($_)" --csharp
 } -ThrottleLimit 100
